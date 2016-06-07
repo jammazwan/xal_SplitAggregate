056e2bd69c34084a2211f36c4b24b4cc582941b8
petecarapetyan <pete@couldbe.net>
2016-06-07 14:18:02 -0500

    initial commit

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+### SplitAggregate NOTES:
+
+ * no notes yet
---
+++ src/test/java/jammazwan/xal/XalTest.java
@@ -0,0 +1,21 @@
+package jammazwan.xal;
+
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.junit.Test;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XalTest extends CamelSpringTestSupport {
+
+    @Override
+    protected AbstractXmlApplicationContext createApplicationContext() {
+        return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
+    }
+
+    @Test
+    public void testXal() throws Exception {
+        String reply = template.requestBody("direct:xal", "No Meaning Here", String.class);
+		assertEquals("My No Meaning Here", reply);
+    }
+
+}
